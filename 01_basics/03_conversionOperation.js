e4a950607fbbb6c57d3fe8a7e3d22cf7403924ac
akshhaaatttt <akshatjain1615@gmail.com>
2024-12-11 14:15:04 +0530

    conversion

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,26 @@
+//let score = "33sgffg"
+//let score = null
+let score = true
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+//NAN not a number
+
+// "33" => 33
+// "33sdf" => NaN
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+
+console.log(stringNumber);
+console.log(typeof(stringNumber));
