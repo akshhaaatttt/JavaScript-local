de98bfa5160a967fe8ca6517945a6bc8b0144cf0
akshhaaatttt <akshatjain1615@gmail.com>
2024-12-11 14:39:24 +0530

    comparision

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,20 @@
+console.log(2>1);
+
+console.log("2" > "1");
+console.log("02" > "1");
+console.log("2" > 1);
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+ // === strict check it check not only the value but also check the dataa type also
+
+ console.log("2" === 2);
+ console.log("2" === "2");
+ console.log("2" === "1");
+ 
