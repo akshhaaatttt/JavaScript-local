bf4c7b918e5425d40c066c08487d50521d27080c
akshhaaatttt <akshatjain1615@gmail.com>
2024-12-19 09:52:51 +0530

    objects 1

--- 02_basics/03_objects.js
+++ 02_basics/03_objects.js
@@ -17,15 +17,27 @@ const Jsuser = {
     lastLogiDays: ["Monday","Saturday"]
 }
 
-console.log(Jsuser.email);
-console.log(Jsuser["email"]);
-//console.log(Jsuser.full name); not able to print the full name now it will only be printed by use of [] this brackets 
-console.log(Jsuser["full name"]);
-console.log(Jsuser[mySym]);//I can only use it as a symbol by applying square bracket
-console.log(typeof Jsuser[mySym]);//I can only use it as a symbol by applying square bracket
+// console.log(Jsuser.email);
+// console.log(Jsuser["email"]);
+// //console.log(Jsuser.full name); not able to print the full name now it will only be printed by use of [] this brackets 
+// console.log(Jsuser["full name"]);
+// console.log(Jsuser[mySym]);//I can only use it as a symbol by applying square bracket
+// //console.log(typeof Jsuser[mySym]);
 
 
+Jsuser.email = "akshat@gmail.com"
+//Object.freeze(Jsuser)
+Jsuser.email = "akshat@yahoo.com"
 
+//console.log(Jsuser);
 
+Jsuser.greeting = function(){
+    console.log("Hello JS user");
+}
+Jsuser.greetingTwo = function(){
+    console.log(`Hello JS user, ${this.name}`);
+}
 
-
+// console.log(Jsuser.greeting);
+console.log(Jsuser.greeting());
+console.log(Jsuser.greetingTwo());
