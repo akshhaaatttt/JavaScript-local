a05960d5cda61c67d74c03e0e77e82dff074a5b0
akshhaaatttt <akshatjain1615@gmail.com>
2024-12-20 22:39:26 +0530

    functions basics

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,35 @@
+
+function sayMyName(){
+console.log("A");
+console.log("k");
+console.log("s");
+console.log("h");
+console.log("a");
+console.log("t");
+}
+//sayMyName()
+
+// function addTwoNumbers(number1,number2/*parameter*/){
+//    console.log( number1+number2);
+   
+// }
+
+// addTwoNumbers(10,11/*arguments*/)
+// addTwoNumbers(10,"11")
+// addTwoNumbers(10,"A")
+// addTwoNumbers(10,null)
+
+// const result = addTwoNumbers(3,5)
+// console.log("Result: ", result);
+
+function addTwoNumbers(number1,number2/*parameter*/){
+
+    // let result = number1+number2
+    // return result
+
+    // return number1+number2
+
+ }
+const result = addTwoNumbers(3,5)
+console.log("Result: ", result);
+
