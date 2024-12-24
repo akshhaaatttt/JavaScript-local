81f8393b2ae535d44f3b627942baa2a99fb68d49
akshhaaatttt <akshatjain1615@gmail.com>
2024-12-24 13:52:43 +0530

    scopes

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -5,13 +5,55 @@ let a = 300
 if(true){/*block scope the value should remains under this scope only */
     let a = 10
     const b = 20
-    console.log("Inner: ",a);
+    //console.log("Inner: ",a);
     
 }
 
 
-console.log(a);
+// console.log(a);
 // console.log(b);
 // console.log(c);
 
+function one(){
 
+    const username = "batman"
+
+    function two(){ // nested function mai scope ke bahar isko access nahi kar sakte par main function ko access kar sakte hai
+        const website = "batman.com"
+        // console.log(username);
+        // console.log(website);
+        
+    }
+    // console.log(website);
+
+    two()
+    
+    
+}
+one()
+
+if (true){
+    const username = "batman"
+    if(username === "batman"){
+        const website = " batman.com"
+        console.log(username + website)
+    }
+    // console.log(website);
+}
+// console.log(username);
+
+
+//+++++++++++++++++++++++++++++++ intresting +++++++++++++++++++++++++
+
+console.log(addone(5))
+function addone(num){
+    return num + 1
+}
+
+
+
+
+const addTwo = function(num){
+    return num + 2
+}
+addTwo(5)
