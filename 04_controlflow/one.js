7ea0e507bc83fc5f201e9c6a2a026e2dacf7040a
akshhaaatttt <akshatjain1615@gmail.com>
2024-12-26 22:54:34 +0530

    one

---
+++ 04_controlflow/one.js
@@ -0,0 +1,21 @@
+//IF
+const isUserloggedIn = true;
+
+if(isUserloggedIn){
+    console.log("hy");
+    
+}
+else{
+
+}
+
+const score = 200
+
+if (score>100){
+    const power = "fly"
+    console.log(`user power: ${power}`);
+    
+}
+// console.log(`user power: ${power}`);
+//console log high
+// viva question
