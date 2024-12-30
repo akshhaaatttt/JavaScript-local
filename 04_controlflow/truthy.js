662686a8de5a00b40271a21e301023f60ab8e862
akshhaaatttt <akshatjain1615@gmail.com>
2024-12-30 22:07:52 +0530

    truthy

---
+++ 04_controlflow/truthy.js
@@ -0,0 +1,17 @@
+const userEmail = []//""//bat@batman.avg"
+
+if (userEmail) {
+    console.log("Got user email");
+    
+} else{
+    console.log("Don't have user email");
+    
+}
+
+
+// falsy values
+
+// false, 0, -0, BigInt 0n, "", null, undefined, Nan
+
+// truthy values
+// "0", 'false', " ", [], {}, function(){}
