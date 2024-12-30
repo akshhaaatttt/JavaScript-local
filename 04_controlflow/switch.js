4f58b7242274f37015d99fb935fb7a6725f02ce8
akshhaaatttt <akshatjain1615@gmail.com>
2024-12-30 22:08:02 +0530

    switch

--- 04_controlflow/switch.js
+++ 04_controlflow/switch.js
@@ -1,1 +1,29 @@
-/* here is the new file of the text */
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = "march"
+
+switch (month) {
+    case "jan":
+        console.log("January");
+        break;
+    case "feb":
+        console.log("feb");
+        break;
+    case "march":
+        console.log("March");
+        break;
+    case 4:
+        console.log("April");
+        break;
+
+    default:
+        console.log("default case match");
+        break;
+}
