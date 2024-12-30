12a2177b0b1f0dfe2d8eecbf801f241d6566061a
akshhaaatttt <akshatjain1615@gmail.com>
2024-12-31 00:00:51 +0530

    truthy

--- 04_controlflow/truthy.js
+++ 04_controlflow/truthy.js
@@ -1,10 +1,10 @@
 const userEmail = []//""//bat@batman.avg"
 
 if (userEmail) {
-    console.log("Got user email");
+    // console.log("Got user email");
     
 } else{
-    console.log("Don't have user email");
+    // console.log("Don't have user email");
     
 }
 
@@ -15,3 +15,34 @@ if (userEmail) {
 
 // truthy values
 // "0", 'false', " ", [], {}, function(){}
+
+// if (userEmail.length === 0) {
+//     console.log("Array is empty");
+    
+// }
+
+const emptyObj = {}
+// Object.keys(emptyObj) : this is used to make a object as a Array
+
+//false == 0:- true
+//false == '':- true
+//0 == '':- true
+
+if (Object.keys(emptyObj).length === 0) {
+    // console.log("Object is empty");
+    
+}
+
+// Nullish Coalescing Operator (??): null undefined
+
+let val1;
+// val1 = 5 ?? 10
+// val1 = null ?? 10
+// val1 = undefined ?? 15 
+val1 = null ?? 10
+
+
+
+
+
+console.log(val1);
