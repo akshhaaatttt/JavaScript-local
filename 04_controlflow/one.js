8aabfde78e659c0d34121656942ed2187a68ed30
akshhaaatttt <akshatjain1615@gmail.com>
2024-12-30 19:09:19 +0530

    one

--- 04_controlflow/one.js
+++ 04_controlflow/one.js
@@ -27,10 +27,32 @@ const balance = 1000
 
 // if(balance > 500) console.log("test"); //implicit scope
 
-if (balance < 500){
-    console.log("less than");    
+// if (balance < 500){
+//     console.log("less than");    
+// }
+
+// else if (balance < 750){
+//     console.log("less than 750");
+    
+// }
+// else if (balance < 900){
+//     console.log("less than 900");
+    
+// }
+// else {
+//     console.log("less than 1200");
+    
+// }
+
+const userLoggedIn = true
+const debitcard = true
+const loggedInFromGoogle = false
+const loggedInFromEmail = true
+if(userLoggedIn && debitcard && 2==2){
+    console.log("Allow to buy course");
 }
 
-else if (balance < 750){
-    
+if(loggedInFromEmail || loggedInFromGoogle){
+    console.log("User logged in");    
 }
+
