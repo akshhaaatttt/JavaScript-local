277fb5259c2b64717a2760d3926ee0d9d62af267
akshhaaatttt <akshatjain1615@gmail.com>
2025-01-01 15:12:33 +0530

    file

--- 04_controlflow/truthy.js
+++ 04_controlflow/truthy.js
@@ -39,14 +39,16 @@ let val1;
 // val1 = 5 ?? 10
 // val1 = null ?? 10
 // val1 = undefined ?? 15 
-val1 = null ?? 10
-
-val2 = null ?? 20
-val2 = null ?? 20
-val2 = null ?? 30
-
-
+val1 = null ?? 10 ?? 20
 
 
 
 console.log(val1);
+
+// Terniary Operator
+
+// condition ? true : false
+
+const iceTeaPrice = 100
+iceTeaPrice <= 80 ? console.log("less than 80") : console.log("more than 80");
+
